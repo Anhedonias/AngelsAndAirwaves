228420e08a959f8b76220d1d4c09b4742c4f47d1
Anhedonia <57368414+Anhedonias@users.noreply.github.com>
2020-02-19 13:07:20 +0300

    Added animation

---
+++ js/main.js
@@ -0,0 +1,82 @@
+'use strict';
+const headerHeight = document.querySelector('.header').offsetHeight;
+
+function smoothScroll(target,duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - headerHeight + 1;
+  var startTime = null;
+  
+  function animation(currentTime){
+    if(startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed,startPosition,distance,duration);
+    window.scrollTo(0,run);
+    if(timeElapsed < duration) requestAnimationFrame(animation);
+  }
+  
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) -1) + b;
+  }
+  
+  requestAnimationFrame(animation);
+}
+
+class NavigationMenu {
+  constructor(root) {
+    this.root = root;
+    this.links = null;
+    this.cacheNodes();
+    this.bindEvents();
+  }
+  
+  cacheNodes() {
+    this.links = this.root.querySelectorAll('.js-page-scroll');
+  }
+  
+  bindEvents() {
+    document.addEventListener('click', (event) => {
+      event.preventDefault();
+      const target = event.target;
+      
+      if (target.classList.contains('js-page-scroll')) {
+        const id = target.hash;
+        
+        smoothScroll(id, 1000);
+      }
+    });
+    
+    window.addEventListener("scroll", event => {
+      let fromTop = window.scrollY + headerHeight + 200;
+
+      this.links.forEach(link => {
+        let section = document.querySelector(link.hash);
+
+        if (
+          section.offsetTop <= fromTop &&
+          section.offsetTop + section.offsetHeight > fromTop
+        ) {
+          link.classList.add("menu__link--active");
+        } else {
+          link.classList.remove("menu__link--active");
+        }
+      });
+    });
+  }
+}
+
+const menuNode = document.querySelector('.js-nav-menu');
+const Menu = new NavigationMenu(menuNode);
+
+$(function(){
+
+	$('.dates-button').on('click', function(e){
+		$('html,body').stop().animate({ scrollTop: $('#tour').offset().top }, 1000);
+		e.preventDefault();
+	});
+	
+	});
